86a28f5d05c02fa52a038653ee28edd9069ff232
k0000k <locker50@naver.com>
2023-05-17 23:07:13 +0900

    Add: project structure

---
+++ src/test/java/com/sscc/capsule/CapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sscc.capsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
