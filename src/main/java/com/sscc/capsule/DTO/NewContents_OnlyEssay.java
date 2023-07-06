c944f261d046d99052af74232bedba4f272cc93f
quizoverflow <jinuinssu@soongsil.ac.kr>
2023-07-06 20:27:27 +0900

    post api 추가

---
+++ src/main/java/com/sscc/capsule/DTO/NewContents_OnlyEssay.java
@@ -0,0 +1,16 @@
+package com.sscc.capsule.DTO;
+
+import java.util.Stack;
+
+public class NewContents_OnlyEssay {
+    private String contents;
+    private String category;
+    private String image; // 수정이 필요할 수도?
+    public String getContents(){
+        return contents;
+    }
+    public String getCategory(){
+        return category;
+    }
+
+}
